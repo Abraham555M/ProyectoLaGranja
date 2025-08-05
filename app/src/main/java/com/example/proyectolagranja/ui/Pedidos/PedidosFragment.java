190609d613d4ed4bfeb55f9962f0df34d9c446b6
Abraham555M <abraham2004hf@gmail.com>
2025-08-05 15:39:55 -0500

    Estructuración del menu con lo que se necesita

---
+++ app/src/main/java/com/example/proyectolagranja/ui/Pedidos/PedidosFragment.java
@@ -0,0 +1,24 @@
+package com.example.proyectolagranja.ui.Pedidos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.proyectolagranja.R;
+public class PedidosFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_pedidos, container, false);
+
+        return rootView;
+    }
+
+
+}
