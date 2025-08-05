9297557870cba9db21f550d8e507e44e96aa9cde
Abraham555M <abraham2004hf@gmail.com>
2025-08-05 17:59:00 -0500

    Avance del diseño

--- app/src/main/java/com/example/proyectolagranja/ui/Pedidos/PedidosFragment.java
+++ app/src/main/java/com/example/proyectolagranja/ui/Pedidos/PedidosFragment.java
@@ -4,21 +4,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.proyectolagranja.R;
-public class PedidosFragment extends Fragment {
-
+public class PedidosFragment extends Fragment implements View.OnClickListener {
+    private Button btnNuevoPedido;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_pedidos, container, false);
 
+        btnNuevoPedido = (Button) rootView.findViewById(R.id.btnNuevoPedido);
+        btnNuevoPedido.setOnClickListener(this);
+
         return rootView;
     }
 
-
+    @Override
+    public void onClick(View v) {
+        if(v == btnNuevoPedido){
+            NavController navController = Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_main);
+            navController.navigate(R.id.action_nav_pedidos_to_nav_catalogo);
+        }
+    }
 }
--- app/src/main/java/com/example/proyectolagranja/ui/Perfil/PerfilFragment.java
+++ app/src/main/java/com/example/proyectolagranja/ui/Perfil/PerfilFragment.java
@@ -14,7 +14,7 @@ public class PerfilFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_pedidos, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_perfil, container, false);
 
         return rootView;
     }
