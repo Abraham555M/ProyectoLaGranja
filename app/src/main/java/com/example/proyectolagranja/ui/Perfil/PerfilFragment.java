533401264f11f4f4ab1fefbfe91cf885577c5efc
Manuel <abraham2004hf@gmail.com>
2025-08-18 09:41:12 -0500

    Implementación del actualizar perfil

--- app/src/main/java/com/example/proyectolagranja/ui/Perfil/PerfilFragment.java
+++ app/src/main/java/com/example/proyectolagranja/ui/Perfil/PerfilFragment.java
@@ -1,21 +1,157 @@
 package com.example.proyectolagranja.ui.Perfil;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.example.proyectolagranja.R;
+import com.example.proyectolagranja.ui.Clases.Venta;
+import com.example.proyectolagranja.ui.Servidor.ServidorConfig;
+import com.google.android.material.textfield.TextInputLayout;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
-public class PerfilFragment extends Fragment {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class PerfilFragment extends Fragment implements View.OnClickListener {
+    private Button btnCambiar, btnGuardarCambios;
+    private EditText etNombresEd, etDocumentoEd, etTelefonoEd, etDireccionEd;
+    private TextInputLayout tilTelefono;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_perfil, container, false);
 
+        etNombresEd = rootView.findViewById(R.id.etNombresEd);
+        etDocumentoEd = rootView.findViewById(R.id.etDocumentoEd);
+        etTelefonoEd = rootView.findViewById(R.id.etTelefonoEd);
+        etDireccionEd = rootView.findViewById(R.id.etDireccionEd);
+        tilTelefono = rootView.findViewById(R.id.tilTelefono);
+
+        // OnClick para cambiar el telefono
+        tilTelefono.setEndIconOnClickListener(v -> onClick(tilTelefono));
+
+        btnGuardarCambios = rootView.findViewById(R.id.btnGuardarCambios);
+        btnGuardarCambios.setOnClickListener(this);
+
+        cargarDatosUsuario();
         return rootView;
+    }
+
+    private void cargarDatosUsuario() {
+        int idCliente = getActivity().getSharedPreferences("DatosUsuario", getActivity().MODE_PRIVATE)
+                .getInt("id_cliente", 1);
+
+        String url = ServidorConfig.URL_SERVIDOR + "cliente/cliente_perfil.php?id_cliente=" + idCliente;
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                try {
+                    etNombresEd.setText(response.getString("nom_cliente"));
+                    etDocumentoEd.setText(response.getString("num_doc_cliente"));
+                    etTelefonoEd.setText(response.getString("tel_cliente"));
+                    etDireccionEd.setText(response.getString("dir_cliente"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(requireContext(), "Error parseando JSON", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                Toast.makeText(getContext(), "Error al conectar con el servidor", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void actualizarDatosUsuario() {
+        int idCliente = requireActivity()
+                .getSharedPreferences("DatosUsuario", requireActivity().MODE_PRIVATE)
+                .getInt("id_cliente", 1);
+
+        String url = ServidorConfig.URL_SERVIDOR + "cliente/cliente_actualizar_perfil.php";
+
+        // Datos que enviaremos
+        RequestParams params = new RequestParams();
+        params.put("id_cliente", idCliente);
+        params.put("nom_cliente", etNombresEd.getText().toString().trim());
+        params.put("dir_cliente", etDireccionEd.getText().toString().trim());
+        params.put("num_doc_cliente", etDocumentoEd.getText().toString().trim());
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.post(url, params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String respuesta = new String(responseBody);
+                if (respuesta.equals("ok")) {
+                    Toast.makeText(requireContext(), "Datos actualizados correctamente", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(requireContext(), "Error al actualizar", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                Toast.makeText(requireContext(), "Error de conexión con el servidor", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void mostrarDialogoCambiarNumero() {
+        LayoutInflater inflater = LayoutInflater.from(requireContext());
+        View dialogView = inflater.inflate(R.layout.alert_dialog_cambiar_numero, null);
+
+        Button btnSi = dialogView.findViewById(R.id.btnCancelarSi);
+        Button btnNo = dialogView.findViewById(R.id.btnCancelarNo);
+
+        // Creamos el AlertDialog
+        androidx.appcompat.app.AlertDialog dialog = new androidx.appcompat.app.AlertDialog.Builder(requireContext())
+                .setView(dialogView)
+                .setCancelable(false) //Si o No
+                .create();
+
+        // Acción botón "Sí"
+        btnSi.setOnClickListener(v -> {
+            dialog.dismiss();
+            // Navegar al otro fragment
+            Navigation.findNavController(requireView())
+                    .navigate(R.id.action_nav_perfil_to_nav_inicio_sesion);
+        });
+
+        // Acción botón "No"
+        btnNo.setOnClickListener(v -> {
+            dialog.dismiss(); // Cerrar el diálogo
+        });
+
+        dialog.show();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == tilTelefono) {
+            mostrarDialogoCambiarNumero();
+        }
+        if(view == btnGuardarCambios){
+            actualizarDatosUsuario();
+        }
     }
 }
